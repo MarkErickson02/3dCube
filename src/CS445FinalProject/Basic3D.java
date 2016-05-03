8d7777460b8899a46f5f61dd51ae274aab36abf9
MarkErickson02 <erickson.mark02@gmail.com>
2016-05-03 14:16:33 -0700

    The program now creates a cube with different colored faces

---
+++ src/CS445FinalProject/Basic3D.java
@@ -0,0 +1,53 @@
+
+package CS445FinalProject;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
+
+public class Basic3D {
+    
+    private DisplayMode displayMode;
+    private FPCamera fp = new FPCamera(0.0f,0.0f,0.0f);
+    
+    public void start(){
+        try{
+            createWindow();
+            initGL();
+            fp.gameLoop();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+      private void createWindow()throws Exception{
+        Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i=0; i<d.length;i++){
+            if (d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32){
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("It's totally minecraft.");
+        Display.create();
+        
+      }
+      
+      private void initGL(){
+        glClearColor(0.0f,0.0f,0.0f,0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float)displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);
+    }
+    
+    public static void main(String[] args){
+        Basic3D basic = new Basic3D();
+        basic.start();
+    }
+}
